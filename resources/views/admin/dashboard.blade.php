fb1b9a57d9c24141063a130b2d95bc8be1c636f9
Arifia Kasastra R <arka.progammer@gmail.com>
2020-12-26 09:39:27 +0700

    Creating base place for Mahasiswa

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+    <h1 class="h2">Dashboard</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group mr-2">
+            <button class="btn btn-sm btn-outline-secondary">Share</button>
+            <button class="btn btn-sm btn-outline-secondary">Export</button>
         </div>
+        <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+            <span data-feather="calendar"></span>
+            This week
+        </button>
+    </div>
+</div>
+<div class="card">
+    <div class="card-body">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        Mahasiswa
     </div>
 </div>
 @endsection
--- resources/views/admin/mahasiswa.blade.php
+++ resources/views/admin/mahasiswa.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-    <h1 class="h2">Dashboard</h1>
-    <div class="btn-toolbar mb-2 mb-md-0">
+    <h1 class="h2">Mahasiswa</h1>
+    {{-- <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group mr-2">
             <button class="btn btn-sm btn-outline-secondary">Share</button>
             <button class="btn btn-sm btn-outline-secondary">Export</button>
@@ -12,7 +12,7 @@
             <span data-feather="calendar"></span>
             This week
         </button>
-    </div>
+    </div> --}}
 </div>
 <div class="card">
     <div class="card-body">
@@ -21,7 +21,41 @@
                 {{ session('status') }}
             </div>
         @endif
-        Mahasiswa
+        
+        <div class="col-md-12">
+            <div class="row">
+                <h3>Data Mahasiswa</h3>
+                <a href="#" style="right: 0px; position: absolute" class="btn btn-primary">
+                    Tambah
+                </a>
+            </div>
+            <div class="table-responsive-md">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">Jumlah Kendaraan</th>
+                            <th scope="col" style="width: 20%">#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>
+                                <a href="#" class="btn btn-warning">Edit</a>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>              
+        </div>
+        
     </div>
 </div>
 @endsection
