c63eda96f666f65a8363186eb834f6e36385bb56
Arifia Kasastra R <arka.progammer@gmail.com>
2020-12-27 20:34:54 +0700

    Showing Mahasiswa dashboard with history

--- app/Http/Controllers/Mahasiswa/MahasiswaController.php
+++ app/Http/Controllers/Mahasiswa/MahasiswaController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Mahasiswa;
 
 use App\Http\Controllers\Controller;
+use App\Kendaraan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MahasiswaController extends Controller
 {
     public function index()
     {
-        return view('mahasiswa.index');
+        return view('mahasiswa.index', [
+            'kendaraans'=>$this->getKendaraan()
+        ]);
+    }
+
+    public function getKendaraan()
+    {
+        return Kendaraan::where('mahasiswa_id', Auth::user()->id)->get();
     }
 }
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -11,7 +11,56 @@
                 {{ session('status') }}
             </div>
         @endif
-        Mahasiswa {{ Auth::user()->nama}}
+        <h4>History Parkir Anda</h4>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Motor</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Waktu</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $vehicle=0;
+                @endphp
+                @foreach ($kendaraans as $kendaraan)
+                    @if (count($kendaraan->histories)!=0)
+                        @for ($i = count($kendaraan->histories)-1; $i > count($kendaraan->histories)-11; $i--)
+                        @php
+                            if($i<0){
+                                break;
+                            }
+                            $vehicle++;
+                            $h = $kendaraan->histories[$i];
+                        @endphp
+                            <tr>
+                                <td>{{$kendaraan->nomor}}</td>
+                                <td>{{$h->tipe==App\History::$KENDARAAN_MASUK ? 'Masuk' : 'Keluar'}}</td>
+                                <td>{{$h->waktu}}</td>
+                            </tr>
+                        @endfor
+                    @endif
+                @endforeach
+                @if ($vehicle==0)
+                    <tr>
+                        <td colspan="2">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card border-danger mb-3 text-center">
+                                        <div class="card-header bg-danger text-white">Tidak ada History Parkir</div>
+                                        <div class="card-body text-danger">
+                                            <i class="fa fa-exclamation-circle" style="font-size: 52pt"></i>
+                                            <p class="card-text">Sepertinya anda belum Pernah Parkir dengan Kendaraan Ini</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                @endif
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
